e937dcd293cf824082acd8b80b2bf5722b1523bd
Muhydeen Adebayo <adediranma@examroom.ai>
2023-07-01 09:18:08 +0100

    Verification implimented

--- SprintApp.Core/Helper/ConstantMessage.cs
+++ SprintApp.Core/Helper/ConstantMessage.cs
@@ -5,5 +5,8 @@
         public const string RegistrationSuccess = "Registration success";
         public const string Unsuccessful = "Unsuccessful request!!!";
         public const string InvalidUser = "Invalid user cridentials";
+        public const string TokenExpired = "Token expired, please retry after sometime";
+        public const string LockedUser = "Account is locked after several failed attempts";
+        public const string CompleteRequest = "Request completed successfully";
     }
 }
